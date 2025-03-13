e4c2464ea4e0be78a2f471f038d551a5c9f1a0bd
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-03-13 16:46:12 +0300

    made changes in gradle

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,6 +3,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("com.android.library")
+    id("com.squareup.sqldelight")
 }
 
 kotlin {
