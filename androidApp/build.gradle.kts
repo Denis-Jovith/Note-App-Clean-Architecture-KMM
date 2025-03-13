8318216b6cfce46a68ee78ce0aa1362ebfa87b4a
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-03-13 17:06:14 +0300

    made changes in gradle

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
-    alias(libs.plugins.compose.compiler)
     id("kotlin-kapt")
-    id("dagger.hilt.android.plugin")
+    id("dagger.hilt.android.plugin") version "2.43.2" // Use the appropriate version
 }
 
 android {
     namespace = "com.example.noteappkmmcleancode.android"
     compileSdk = 34
+
     defaultConfig {
         applicationId = "com.example.noteappkmmcleancode.android"
         minSdk = 24
@@ -16,40 +16,60 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
     buildFeatures {
         compose = true
     }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+        isCoreLibraryDesugaringEnabled = true
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation(projects.shared)
+    implementation(project(":shared"))
+
+    // Jetpack Compose
     implementation(libs.compose.ui)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.compose.material3)
     implementation(libs.androidx.activity.compose)
     debugImplementation(libs.compose.ui.tooling)
 
+    // Core Library Desugaring
+    coreLibraryDesugaring(libs.desugar.jdk.libs)
 
+    // KotlinX Datetime
     implementation(libs.kotlinx.datetime)
-    implementation(libs.hilt.android.compiler)
-    implementation(libs.androidx.x.x.x)
-    implementation(libs.androidx.hilt.navitation.compose)
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // AndroidX Dependencies
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Hilt Navigation Compose
+    implementation(libs.androidx.hilt.navigation.compose)
+    coreLibraryDesugaring (libs.desugar.jdk.libs)
 
 }
