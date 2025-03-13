adb85ee426b94a6a47644d9863f91653e3652144
Denis-Jovith <denisjovitusbuberwa@gmail.com>
2025-03-13 17:32:08 +0300

    made changes in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
-    //trick: for the same plugin versions in all sub-modules
+    // Apply Android and Kotlin plugins globally but only in sub-modules
     alias(libs.plugins.androidApplication).apply(false)
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+
+    // Compose plugin for Compose-based UI projects
     alias(libs.plugins.compose.compiler).apply(false)
+
 }
